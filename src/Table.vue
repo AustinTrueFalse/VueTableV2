a0a780808a1f398fddfc8ae56fa368c4153578fc
craash00077@gmail.com <lavenetsky.ma@gmail.com>
2023-06-27 23:13:29 +0300

    updated data

--- src/Table.vue
+++ src/Table.vue
@@ -4,27 +4,27 @@ import BaseTable from './components/Table/BaseTable.vue';
 import TableRow from './components/Table/TableRow.vue';
 import TableColumn from './components/Table/TableColumn.vue';
 
-const tableHeads = ['Id', 'Author', 'Title', 'Cost', '']
-const tableSizeColumns = '50px 1fr 2fr 150px 140px'
+const tableHeads = ['Id', 'Action', 'Descr', 'Summa', '']
+const tableSizeColumns = '100px 1fr 2fr 150px 140px'
 
 const books = ref([
   {
-    id: 1,
-    author: 'Dmitry Glukhovsky',
-    title: 'Metro 2033',
-    cost: 500,
+    id: 12345678,
+    action: '30 % Помидоры',
+    descr: 'Баллы за покупку',
+    summa: 500,
   },
   {
-    id: 12,
-    author: 'James Clear',
-    title: 'Atomic Habits: An Easy',
-    cost: 600,
+    id: 12345679,
+    action: '50% Хлеб',
+    descr: 'Баллы за категорию в чеке',
+    summa: 600,
   },
   {
-    id: 2,
-    author: 'J. K. Rowling',
-    title: 'Harry Potter and the Order of the Phoenix',
-    cost: 1000,
+    id: 12345690,
+    action: '50% Огурцы',
+    descr: 'Баллы за визит',
+    summa: 1000,
   }
 ])
 
@@ -47,13 +47,13 @@ const books = ref([
         {{ book.id }}
       </TableColumn>
       <TableColumn>
-        {{ book.author }}
+        {{ book.action }}
       </TableColumn>
       <TableColumn>
-        {{ book.title }}
+        {{ book.descr }}
       </TableColumn>
       <TableColumn>
-        {{ book.cost }}
+        {{ book.summa }}
       </TableColumn>
     </TableRow>
   </BaseTable>
